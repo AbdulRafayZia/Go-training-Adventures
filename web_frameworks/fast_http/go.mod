32ea1127e3f12b75720bcc8b8b67198f2dd98d03
Abdul Rafay Zia <rafayzia3690@gmail.com>
2023-11-16 14:12:40 +0500

    get slice using gin

---
+++ web_frameworks/fast_http/go.mod
@@ -0,0 +1,10 @@
+module fastHttp.go
+
+go 1.21.1
+
+require (
+	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/klauspost/compress v1.17.3 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+)
--- web_frameworks/gin/example.go
+++ web_frameworks/gin/example.go
@@ -1,3 +1,32 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+func main() {
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
+
+    router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
+}
 // package main
 
 // import (
@@ -7,34 +36,16 @@
 // )
 
 // func main() {
-//   r := gin.Default()
-//   r.GET("/ping", func(c *gin.Context) {
-//     c.JSON(http.StatusOK, gin.H{
-//       "message": "pong",
+//     // Create a new Gin router
+//     router := gin.Default()
+
+//     // Define a route and handler for the "/" path
+//     router.GET("/", func(c *gin.Context) {
+//         c.JSON(http.StatusOK, gin.H{
+//             "message": "Hello world!",
+//         })
 //     })
-//   })
-//   r.Run( ) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+//     // Start the server on port 8080
+//     router.Run(":8080")
 // }
-
-package main
-
-import (
-  "net/http"
-
-  "github.com/gin-gonic/gin"
-)
-
-func main() {
-    // Create a new Gin router
-    router := gin.Default()
-
-    // Define a route and handler for the "/" path
-    router.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Hello world!",
-        })
-    })
-
-    // Start the server on port 8080
-    router.Run(":8080")
-}
