ea2b1e31d8c70cccb46f7d7568dca4298a2fa7d3
Abdul Rafay Zia <rafayzia3690@gmail.com>
2023-11-16 12:16:31 +0500

    changes in echo

--- web_frameworks/echo/echo.go
+++ web_frameworks/echo/echo.go
@@ -6,10 +6,24 @@ import (
     "github.com/labstack/echo/v4"
 )
 
+type User struct {
+    Name  string `json:"name" xml:"name" form:"name" query:"name"`
+    Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
 func main() {
     e := echo.New()
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello, World!")
     })
     e.Logger.Fatal(e.Start(":1323"))
+    e.POST("/users", func(c echo.Context) error {
+        u := new(User)
+        if err := c.Bind(u); err != nil {
+            return err
+        }
+        return c.JSON(http.StatusCreated, u)
+        // or
+        // return c.XML(http.StatusCreated, u)
+    })
 }
